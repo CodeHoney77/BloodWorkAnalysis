465e899fbb6c1ec1dffea975714c028a34d0c23e
anthony huang <Anthony>
2024-05-17 15:17:38 +1000

    Actually defined dataset variables

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -1,1 +1,8 @@
-View(MasterDOMINO)
+# Defining variables
+MSBlood <- read_excel("C:/Users/ah437/OneDrive/Documents/Work/MasterDOMINO.xlsx", sheet = "MS Blood")
+MSUrine <- read_excel("C:/Users/ah437/OneDrive/Documents/Work/MasterDOMINO.xlsx", sheet = "MS Urine")
+NMRBloodBin <- read_excel("C:/Users/ah437/OneDrive/Documents/Work/MasterDOMINO.xlsx", sheet = "NMR Blood bin")
+NMRUrineBin <- read_excel("C:/Users/ah437/OneDrive/Documents/Work/MasterDOMINO.xlsx", sheet = "NMR Urine bin")
+NMRBloodmM <-  read_excel("C:/Users/ah437/OneDrive/Documents/Work/MasterDOMINO.xlsx", sheet = "NMR Blood mM")
+
+# You will have to change file path depending on where you saved them 
